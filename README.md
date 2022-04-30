96f9fe3964e7215d377f210135539c8e9759d6a8
lotus <daniel.younghwan.lee@gmail.com>
2022-04-30 16:06:14 -0400

    challenge complete

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # Password Generator Starter Code
 
-The purpose of this challenge is to apply our new knowledge and cement our understanding of foundational JavaScript. We are asked to set up the criteria selection for generating a secure password. Through the usage of various JavaScript elements such as functions, conditionals, and variables, we are able to achieve this task.
+The purpose of this challenge is to apply our new knowledge and cement our understanding of foundational JavaScript. We are asked to set up the criteria selection for generating a secure password. Through the usage of various JavaScript elements such as functions, conditionals, and variables, among others we are able to achieve this task.
 
-After setting up my repository workflow, figuring out what to do first was already a challenge. There was a bit of uncertainty about certain aspects of the Acceptance Criteria. Once I figured out how to apply prompts, then came the challenge of translating the prompt inputs into something that would actually generate a password.
+This challenge was difficult to begin at first. It took awhile for me to grasp the logic behind JavaScript. It was only once I began to understand the link between various parts that my confidence grew and I was able to make progress.
 
-Screenshot of homepage (before password is generated):
+Here is a list of noteworthy observations I made during this challenge:
+- Whenever possible, use "console.log".  As a beginner, it is important that you keep track of what is happening.
+- Provide relevant names for functions, variables, and arrays. On that note, provide comments whenever you feel necessary. It is important to understand what a certain block of code is referring to both for your own sake and that of others.
+- There are many interesting functionalities that you can unlock once you grasp the basics (for instance, being able to combine arrays).
+- The order in which you place the code is important both for the computer reading it and for yourself in understanding it.
+- In JavaScript, there may be multiple correct answers to a certain problem. It is on you to figure out not only the best one but the one that makes sense to you.
+
+Overall, this challenge was definitely worth the time it took. I learned a lot through trial and error and was forced to conduct my own research throughout the process. Here is the URL link where you can test the password generation: 
+
+Screenshot of homepage before password is generated:
 
 ![Alt text](/homepage-screenshot.PNG)
+
+Screenshot of homepage after password is generated:
+
+![Alt text](/homepage-password.PNG)
+
+Screenshot of console log for previous shown password:
+
+![Alt text](/console-log.PNG)
--- script.js
+++ script.js
@@ -4,73 +4,83 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// list of available characters
+var lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var numericArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var specialArray = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '^', '_', '`', '{', '|', '}', '~']
+var emptyArray = [];
+var passwordArray = [];
+
 function generatePassword() {
   // generateLength is the length of password, it is an integer
   var generateLength = window.prompt("How many characters would you like your password to contain?");
   if (generateLength < 8) {
     window.alert("Password must be greater than 7 characters.")
     generatePassword();
+    //return
   }
   if (generateLength > 128) {
     window.alert("Password must be less than 129 characters.")
     generatePassword();
+    //return
   }
   generateLength = parseInt(generateLength);
   console.log(generateLength);
-  generateCharacters();
+  return generateCharacters(generateLength);
+}
 
-  // list of available characters
-  function generateCharacters() {
-    var lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    var numericArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    var specialArray = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '^', '_', '`', '{', '|', '}', '~']
+function generateCharacters(generateLength) {
+  // prompts to determine password criteria
+  var lowerCase = window.confirm("Click OK to confirm including lower case characters.");
+  console.log(lowerCase);
+  var upperCase = window.confirm("Click OK to confirm including upper case characters.");
+  console.log(upperCase);
+  var numeric = window.confirm("Click OK to confirm including numeric characters.");
+  console.log(numeric);
+  var special = window.confirm("Click OK to confirm including special characters.");
+  console.log(special);
 
-    // prompts to determine password criteria
-    var lowerCase = window.confirm("Click OK to confirm including lower case characters.");
-    console.log(lowerCase);
-    var upperCase = window.confirm("Click OK to confirm including upper case characters.");
-    console.log(upperCase);
-    var numeric = window.confirm("Click OK to confirm including numeric characters.");
-    console.log(numeric);
-    var special = window.confirm("Click OK to confirm including special characters.");
-    console.log(special);
-
-    // conditionals based on character prompt responses
-    if (lowerCase === false && upperCase === false && numeric === false && special === false) {
-      window.alert("You must select at least one character type.")
-      generateCharacters();
-    }
-    // how to loop these if true??
-    if (lowerCase === true) {
-      var lowerCaseRandom = lowerCaseArray[Math.floor(Math.random() * lowerCaseArray.length)];
-      console.log(lowerCaseRandom);
-    }
-    if (upperCase === true) {
-      var upperCaseRandom = upperCaseArray[Math.floor(Math.random() * upperCaseArray.length)];
-      console.log(upperCaseRandom);
-    }
-    if (numeric === true) {
-      var numericRandom = numericArray[Math.floor(Math.random() * numericArray.length)];
-      console.log(numericRandom);
-    }
-    if (special === true) {
-      var specialRandom = specialArray[Math.floor(Math.random() * specialArray.length)];
-      console.log(specialRandom);
-    }
-    // how do we collect these random generated characters? and then how do we random those??? do we create another array?
-    // var storedPassword;
+  // conditionals based on character prompt responses
+  if (lowerCase === false && upperCase === false && numeric === false && special === false) {
+    window.alert("You must select at least one character type.")
+    generateCharacters();
   }
+
+  if (lowerCase === true) {
+    var combinedArray = emptyArray.concat(lowerCaseArray);
+  }
+  if (upperCase === true) {
+    var combinedArray = emptyArray.concat(combinedArray, upperCaseArray);
+  }
+  if (numeric === true) {
+    var combinedArray = emptyArray.concat(combinedArray, numericArray);
+  }
+  if (special === true) {
+    var combinedArray = emptyArray.concat(combinedArray, specialArray);
+  }
+  console.log(combinedArray);
+
+  // generating potential password
+  for (let i = 0; i < generateLength; i++) {
+    var randomizer = combinedArray[Math.floor(Math.random() * combinedArray.length)];
+    console.log(randomizer);
+    passwordArray.push(randomizer);
+  }
+  console.log(passwordArray);
+  var possiblePassword = passwordArray.join('');
+  console.log(possiblePassword);
+
+  return possiblePassword;
 }
 
 // Write password to the #password input
 function writePassword() {
 
   var password = generatePassword(); // return a string which should be the final random password
+  console.log(password);
   var passwordText = document.querySelector("#password");
-  console.log(passwordText)
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
